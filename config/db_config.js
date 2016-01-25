3a2ad8590aaa3f43388847e4abbe9868f0e013d4
Luiz Calor Zanini <zanini@za9.com.br>
2016-01-25 16:51:42 -0200

    Testando mongo no mongolab

--- config/db_config.js
+++ config/db_config.js
@@ -1,5 +1,7 @@
 
-var db_string = "mongodb://localhost:27017/modelo_restfull";
+//var db_string = "mongodb://localhost:27017/modelo_restfull";
+var db_string = "mongodb://zanini:lczm798585@ds049935.mongolab.com:49935/apinodejs";
+
 
 var mongoose = require('mongoose').connect(db_string);
 
