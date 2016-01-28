c59282998525203a5bd61a2d0949ec6c451e3534
Luiz Calor Zanini <zanini@za9.com.br>
2016-01-28 21:57:58 -0200

    Remove morgan

--- config/app_config.js
+++ config/app_config.js
@@ -1,10 +1,6 @@
 
 var express = require('express');
-var morgan = require('morgan'); //logs
-var fs = require('fs');  // file system
-
 var app = module.exports = express();
-
 var bodyParser = require('body-parser');
 
 var allowCors = function(req, res, next){
@@ -16,14 +12,9 @@ var allowCors = function(req, res, next){
 	next();
 };
 
-//Salvando log
-var accessLogStream = fs.createWriteStream(__dirname + '/../log/access.log', {flags: 'a'});
-app.use(morgan('combined', {stream: accessLogStream}));
-
 app.listen(5000);
 app.use(allowCors);
 
 //Acionando o Json com BodyParse
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-
